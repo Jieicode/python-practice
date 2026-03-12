0e4eb4eb582ffec83e166e03347c9dab6902cef8
jiei nakagawa <jiei1166.1212@gmail.com>
2026-03-12 21:06:14 +0900

    add hint function to number game

--- number_game.py
+++ number_game.py
@@ -3,5 +3,7 @@ answer = random.randint(1,10)
 guess = int(input("1～10の数字を当ててください:"))
 if guess == answer:
     print("正解！")
+elif guess > answer:
+    print("もっと小さい")
 else:
-    print("はずれ！　正解は", answer)
+    print("もっと大きい")
