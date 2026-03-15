f64a5c1a6830c4e310403f78a0baa882af237e29
jiei nakagawa <jiei1166.1212@gmail.com>
2026-03-15 09:23:54 +0900

    improve number game

--- number_game.py
+++ number_game.py
@@ -1,9 +1,15 @@
 import random
 answer = random.randint(1,10)
-guess = int(input("1～10の数字を当ててください:"))
-if guess == answer:
-    print("正解！")
-elif guess > answer:
-    print("もっと小さい")
-else:
-    print("もっと大きい")
+count = 0
+while count < 5:
+    guess = int(input("1～10の数字を当ててください:"))
+    count = count + 1
+    if guess == answer:
+        print("正解！")
+        break
+    elif guess > answer:
+        print("もっと小さい")
+    else:
+        print("もっと大きい")
+if count == 5 and guess != answer:
+    print("ゲームオーバー　正解は", answer)
