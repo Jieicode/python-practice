e84683ac7bfb9ad817d104426c8b7c6c7bb6df89
jiei nakagawa <jiei1166.1212@gmail.com>
2026-03-18 17:52:59 +0900

    add pkay again feature

--- number_game.py
+++ number_game.py
@@ -1,15 +1,19 @@
 import random
-answer = random.randint(1,10)
-count = 0
-while count < 5:
-    guess = int(input("1～10の数字を当ててください:"))
-    count = count + 1
-    if guess == answer:
-        print("正解！")
-        break
-    elif guess > answer:
-        print("もっと小さい(あと", 5-count, "回)")
-    else:
-        print("もっと大きい(あと", 5-count, "回)")
+play = "y"
+while play =="y":
+    answer = random.randint(1,10)
+    count = 0
+    while count < 5:
+       guess = int(input("1～10の数字を当ててください:"))
+       count = count + 1
+       if guess == answer:
+           print("正解！", count, "回で当てました！")
+           break
+       elif guess > answer:
+           print("もっと小さい(あと", 5-count, "回)")
+       else:
+           print("もっと大きい(あと", 5-count, "回)")
 if count == 5 and guess != answer:
     print("ゲームオーバー　正解は", answer)
+
+play = input("もう一回遊びますか? y/n: ")
